c533246926ec4f1c7ceed9cd5605735298bdb2ed
Your Name <you@example.com>
2019-08-29 11:59:51 +0800

    黑马头条PC-统一全局注册插件的使用方式Vue.use

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.css'
 import axios from 'axios'
+import Component from './components'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.use(ElementUI)
+Vue.use(Component)
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
-
 new Vue({
   router,
   render: h => h(App)
