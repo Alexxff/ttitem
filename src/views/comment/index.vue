4ba72d7fd18c044a2ba5cedca9ee29ca12b65bf7
Your Name <you@example.com>
2019-09-01 00:27:34 +0800

    评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -12,10 +12,17 @@
       <el-table-column label="操作">
         <template slot-scope=" obj ">
           <el-button type="text">修改</el-button>
-          <el-button @click="closeOrOpen(obj.row)"  :style="{color: obj.row.comment_status ?'#E6A23C' : '#409EFF'}" type="text">{{obj.row.comment_status?"关闭评论":"打开评论"}}</el-button>
+          <el-button
+            @click="closeOrOpen(obj.row)"
+            :style="{color: obj.row.comment_status ?'#E6A23C' : '#409EFF'}"
+            type="text"
+          >{{obj.row.comment_status?"关闭评论":"打开评论"}}</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-row type="flex" justify="center" style="margin:20px 0">
+      <el-pagination @current-change="changePage" :page-size="page.pageSize" :current-page="page.currentPage" background layout="prev, pager, next" :total="page.total"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -23,10 +30,19 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        pageSize: 10,
+        total: 0,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getComments()
+    },
     closeOrOpen (row) {
       let mess = row.comment_status ? '关闭' : '打开'
       this.$confirm(`您确定要${mess}评论吗`, '提示').then(() => {
@@ -40,15 +56,22 @@ export default {
         })
       })
     },
-    // 查询列表
+    // 查询列表,查询数据
     getComments () {
+      let pageParams = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
       this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          ...pageParams
+
         }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     },
     formatter (rom, column, cellValue, index) {
