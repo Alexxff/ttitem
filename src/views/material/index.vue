39fff8dc75bad87cc78742e30feb863b63ce5a97
Your Name <you@example.com>
2019-09-01 16:22:44 +0800

    素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,9 +3,26 @@
     <bread-crumb sot=:header>
     <template slot="title">素材管理</template>
     </bread-crumb>
-    <el-tabs v-model="activeName">
-    <el-tab-pane label="全部图片" name="first">全部图片</el-tab-pane>
-    <el-tab-pane label="收藏图片" name="second">收藏图片</el-tab-pane>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+    <el-tab-pane label="全部图片" name="all">
+      <div class='img-list'>
+        <el-card class='img-card' v-for="item in list" :key="item.id">
+          <img :src="item.url" alt="">
+          <el-row align="middle" class='operate' type='flex' justify='space-around'>
+            <i class='el-icon-star-on'></i>
+            <i class='el-icon-delete-solid'></i>
+          </el-row>
+        </el-card>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="收藏图片" name="collect">
+      <div class='img-list'>
+        <el-card class='img-card' v-for="item in list" :key="item.id">
+          <img :src="item.url" alt="">
+
+        </el-card>
+      </div>
+    </el-tab-pane>
   </el-tabs>
   </el-card>
 </template>
@@ -14,13 +31,55 @@
 export default {
   data () {
     return {
-      activeName: 'first'
+      activeName: 'all',
+      list: []
     }
+  },
+  methods: {
+    changeTab () {
+      // console.log('chu')
+      this.getMaterial()
+    },
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: { collect: this.activeName === 'collect' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
   }
-
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.img-list{
+  display:flex;
+  justify-content:space-around;
+  flex-wrap:wrap;
+  .img-card{
+    width: 200px;
+    height:200px;
+    margin:20px 10px;
+    position: relative;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+    .operate{
+      position: absolute;
+      bottom:0;
+      left:0;
+      height: 30px;
+      width: 100%;
+      background-color: #f4f5f6;
+      i{
+        font-size:22px;
+      }
+    }
+  }
+}
 </style>
