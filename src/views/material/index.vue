889b2aae4eb8acc61f097f932f48abdf1118d749
Your Name <you@example.com>
2019-09-01 19:55:19 +0800

    分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,29 +1,36 @@
 <template>
   <el-card>
-    <bread-crumb sot=:header>
-    <template slot="title">素材管理</template>
+    <bread-crumb sot=":header">
+      <template slot="title">素材管理</template>
     </bread-crumb>
     <el-tabs v-model="activeName" @tab-click="changeTab">
-    <el-tab-pane label="全部图片" name="all">
-      <div class='img-list'>
-        <el-card class='img-card' v-for="item in list" :key="item.id">
-          <img :src="item.url" alt="">
-          <el-row align="middle" class='operate' type='flex' justify='space-around'>
-            <i class='el-icon-star-on'></i>
-            <i class='el-icon-delete-solid'></i>
-          </el-row>
-        </el-card>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="收藏图片" name="collect">
-      <div class='img-list'>
-        <el-card class='img-card' v-for="item in list" :key="item.id">
-          <img :src="item.url" alt="">
+      <el-tab-pane label="全部图片" name="all">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row align="middle" class="operate" type="flex" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
 
-        </el-card>
-      </div>
-    </el-tab-pane>
-  </el-tabs>
+        </div>
+            <el-row type='flex' justify='center'>
+            <el-pagination :current-page="page.currentPage" :page-size="page.pageSize" :total="page.total" @current-change="changePage" background layout="prev, pager, next"></el-pagination>
+          </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+
+        </div>
+        <el-row type='flex' justify='center'>
+            <el-pagination :current-page="page.currentPage" :page-size="page.pageSize" :total="page.total" @current-change="changePage" background layout="prev, pager, next"></el-pagination>
+          </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -32,20 +39,35 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        currentPage: 1,
+        pageSize: 15,
+        total: 0
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
     changeTab () {
       // console.log('chu')
+      this.page.currentPage = 1
       this.getMaterial()
     },
     getMaterial () {
+      let pageParams = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect', ...pageParams }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -56,28 +78,28 @@ export default {
 </script>
 
 <style lang='less' scoped>
-.img-list{
-  display:flex;
-  justify-content:space-around;
-  flex-wrap:wrap;
-  .img-card{
+.img-list {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  .img-card {
     width: 200px;
-    height:200px;
-    margin:20px 10px;
+    height: 200px;
+    margin: 20px 10px;
     position: relative;
-    img{
+    img {
       width: 100%;
       height: 100%;
     }
-    .operate{
+    .operate {
       position: absolute;
-      bottom:0;
-      left:0;
+      bottom: 0;
+      left: 0;
       height: 30px;
       width: 100%;
       background-color: #f4f5f6;
-      i{
-        font-size:22px;
+      i {
+        font-size: 22px;
       }
     }
   }
